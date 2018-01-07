05592d081f4b536a6ae05c2b771c9a81d2d6f15b
Marketionist <marketionist@gmail.com>
2018-01-07 23:59:41 +0200

    Fixed JSON parsing, added semicolons

--- specs/playerPageTCs.js
+++ specs/playerPageTCs.js
@@ -1,15 +1,15 @@
-describe('Player Page Tests', ()=> {
+describe('Player Page Tests', () => {
 
     // let searchField = $('[placeholder="Enter search keywords"]')
-    let playerFullName = global.importedJSON.playerFullName()
-    let playercountry = global.importedJSON.playerCountryName()
-    
-    it('Player Name matches feed', ()=> {
-        browser.get('players/player.08793.tiger-woods.html')
+    let playerFullName = global.importedJSON.playerFullName;
+    let playerCountry = global.importedJSON.playerCountryName;
 
-        let displayName = $('#playersListContainer').$('option[value*="08793.png"]').getText()
-        
-       
-        expect(displayName).toContain(playerFullName, 'Displayed Player Name does not match to feed')
+    it('Player Name matches feed', () => {
+
+        browser.get('players/player.08793.tiger-woods.html');
+
+        let displayName = $('#playersListContainer').$('option[value*="08793.png"]').getText();
+
+        expect(displayName).toContain(playerFullName, 'Displayed Player Name does not match to feed');
     })
 })
